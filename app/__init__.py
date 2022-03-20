1b990f8a00d38bdc96b370cb09973050166c77d8
Peter Cardenas <petercardenas441@gmail.com>
2022-03-20 01:09:40 -0500

    feat: webserver with dummy code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import random
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-  return "<p>Hello, World!</p>"
+  arr_len = request.args.get("arrlen", default=2500, type=int)
+  num_itrs = request.args.get("itrs", default=1000, type=int)
+  start = time.time()
+  arr = list(range(arr_len))
+  for _ in range(num_itrs):
+    random.shuffle(arr)
+    arr.sort()
+  duration = time.time() - start
+  return f"<p>Shuffled a %d length array for %d iterations. Took %s seconds</p>" % (arr_len, num_itrs, duration)
